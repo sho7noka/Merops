67ecb9afe9dce52e69b4d550229560c523a794da
sho7noka <alia_japan7@yahoo.co.jp>
2019-01-10 22:29:21 +0900

    update

---
+++ Merops/engine/CoreModel.h
@@ -0,0 +1,13 @@
+//
+//  CoreModel.h
+//  Merops
+//
+//  Created by sho sumioka on 2019/01/09.
+//  Copyright © 2019 sho sumioka. All rights reserved.
+//
+
+#ifndef CoreModel_h
+#define CoreModel_h
+
+
+#endif /* CoreModel_h */
---
+++ Merops/engine/CoreModel.m
@@ -0,0 +1,9 @@
+//
+//  CoreModel.m
+//  Merops
+//
+//  Created by sho sumioka on 2019/01/09.
+//  Copyright © 2019 sho sumioka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
